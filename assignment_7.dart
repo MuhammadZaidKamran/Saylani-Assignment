81c84f55288511f0bfe4d9323fe0f0c7e08e81c3
MuhammadZaidKamran <155019314+MuhammadZaidKamran@users.noreply.github.com>
2024-01-01 15:52:03 +0500

    Update assignment_7.dart

--- assignment_7.dart
+++ assignment_7.dart
@@ -2,12 +2,8 @@
 import 'dart:math';
 
 void main() {
-  num number1 = ;
+  num number1 = 6;
   num number2 = sqrt(number1);
 
   print("Root of $number1 is $number2");
-
-  if (number1 == null) {
-    print("Value Invalid");
-  }
 }
